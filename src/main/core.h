02b97723f60df7a56f294a0e5f1b0fdf9d7d97eb
Timothy Stark <fsword007@gmail.com>
2022-04-15 13:51:47 -0400

    Orbital Flight Simulator - initial commit

---
+++ src/main/core.h
@@ -0,0 +1,14 @@
+// core.h - main core definitions package
+//
+// Author:  Tim Stark
+// Date:    Apr 15, 2022
+
+#define OFS_APPNAME     "Orbital Flight Simulator"
+#define OFS_APPSHORT    "OFS"
+
+// Default screen coordination size
+#define SCR_WIDTH       1920
+#define SCR_HEIGHT      1080
+
+#include <iostream>
+
