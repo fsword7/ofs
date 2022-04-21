b0987ed30c2a10aaaacb22ddb769f6089e94df72
Timothy Stark <fsword007@gmail.com>
2022-04-21 09:49:22 -0400

    Planetary system package

--- src/universe/body.cpp
+++ src/universe/body.cpp
@@ -5,3 +5,70 @@
 
 #include "main/core.h"
 #include "universe/body.h"
+#include "universe/astro.h"
+
+// ******** Planetary System ********
+
+PlanetarySystem::PlanetarySystem(celBody *body)
+: body(body)
+{
+    bodies.clear();
+}
+
+PlanetarySystem::PlanetarySystem(celStar *star)
+: star(star)
+{
+    bodies.clear();
+}
+
+void PlanetarySystem::addBody(celBody *body)
+{
+    bodies.push_back(body);
+    body->setInSystem(this);
+}
+
+void PlanetarySystem::removeBody(celBody *body)
+{
+}
+
+celBody *PlanetarySystem::find(cstr_t &name) const
+{
+    for (auto body : bodies)
+    {
+        if (name == body->getsName())
+            return body;
+    }
+    return nullptr;
+}
+
+// ******** Celestial Body ********
+
+PlanetarySystem *celBody::createPlanetarySystem()
+{
+    if (ownSystem != nullptr)
+        return ownSystem;
+
+    ownSystem = new PlanetarySystem(this);
+    ownSystem->setStar(inSystem->getStar());
+    return ownSystem;
+}
+
+double celBody::getLuminosity(double lum, double dist) const
+{
+    double power = lum * SOLAR_POWER;
+    double irradiance = power / sphereArea(dist * 1000.0);
+    double incidentEnergy = irradiance * circleArea(radius * 1000.0);
+    double reflectedEnergy = incidentEnergy * albedo;
+
+    return reflectedEnergy / SOLAR_POWER;
+}
+
+double celBody::getApparentMagnitude(vec3d_t sun, double irradiance, vec3d_t view) const
+{
+    double vdist  = glm::length(view);
+    double sdist  = glm::length(sun);
+    double illum  = (1.0 * glm::dot(view / vdist, sun / sdist)) / 2.0;
+    double absMag = getLuminosity(irradiance, sdist) * illum;
+
+    return astro::convertLumToAppMag(absMag, astro::convertKilometerToParsec(vdist));
+}
--- src/universe/body.h
+++ src/universe/body.h
@@ -7,11 +7,96 @@
 
 #include "engine/object.h"
 
+class celStar;
+class celBody;
+
+class PlanetarySystem
+{
+public:
+    PlanetarySystem(celBody *body = nullptr);
+    PlanetarySystem(celStar *star);
+    ~PlanetarySystem() = default;
+
+    inline celBody *getPrimaryBody()        { return body; }
+    inline celStar *getStar()               { return star; }
+    
+    inline void setStar(celStar *nStar)     { star = nStar; }
+
+    void addBody(celBody *body);
+    void removeBody(celBody *body);
+
+    celBody *find(cstr_t &name) const;
+
+private:
+    // Planetary system parameters
+    // System *solarSystem = nullptr;
+    celBody *body = nullptr;
+    celStar *star = nullptr;
+
+    std::vector<celBody *> bodies;
+};
+
+enum celType
+{
+    cbUnknown = 0,
+    cbStar,
+    cbPlanet,
+    cbDwarfPlanet,
+    cbMoon,
+    cbAsteroid,
+    cbComet
+};
+
 class celBody : public Object
 {
 public:
-    celBody() = default;
-    ~celBody() = default;
+    enum {
+        cbKnownRadius   = 1,
+        cbKnownRotation = 2,
+        cbKnownSurface  = 4,
+        cbKnownObject   = (cbKnownRadius|cbKnownRotation|cbKnownSurface)
+    };
 
+    celBody(PlanetarySystem *system, cstr_t &name, celType type)
+    : Object(name, objCelestialBody), cbType(type), inSystem(system)
+    {
+        inSystem->addBody(this);
+    }
 
+    celBody(cstr_t &name, celType type, celBody *body = nullptr)
+    : Object(name, objCelestialBody), cbType(type)
+    {
+        if (body != nullptr)
+        {
+            inSystem = body->createPlanetarySystem();
+            inSystem->addBody(this);
+        }
+    }
+
+    virtual ~celBody() = default;
+
+    inline celType getCelestialType() const { return cbType; }
+
+    inline void setInSystem(PlanetarySystem *system) { inSystem = system; }
+
+    inline PlanetarySystem *getOwnSystem() const    { return ownSystem; }
+    inline PlanetarySystem *getInSystem() const     { return inSystem; }
+
+    inline void setColor(color_t nColor)            { color = nColor; }
+    inline color_t getColor() const                 { return color; }
+
+    PlanetarySystem *createPlanetarySystem();
+
+    double getLuminosity(double lum, double dist) const;
+    double getApparentMagnitude(vec3d_t sun, double irradiance, vec3d_t view) const;
+    
+private:
+    celType cbType = cbUnknown;
+
+protected:
+    color_t color = color_t(0.7f, 0.7f, 0.7f, 1.0f);
+    uint32_t knownFlags = 0;
+
+    PlanetarySystem *ownSystem = nullptr;
+    PlanetarySystem *inSystem = nullptr;
 };
