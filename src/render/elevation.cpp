f8b71698764b2cad7d7b807457d16b065ab6bbf6
Timothy Stark <fsword007@gmail.com>
2022-04-25 09:09:48 -0400

    Elevation data package (not fully implemented yet).

---
+++ src/render/elevation.cpp
@@ -0,0 +1,10 @@
+// elevation.cpp - Elevation data package
+//
+// Author:  Tim Stark
+// Date:    Apr 25, 2022
+
+#include "main/core.h"
+#include "osd/gl/mesh.h"
+#include "render/scene.h"
+#include "render/surface.h"
+
