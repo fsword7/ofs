56e439b8b7d417b041a01da0569d51c326ebaa14
Timothy Stark <fsword007@gmail.com>
2022-04-25 08:45:00 -0400

    Orbital elements package for spacecrafts

---
+++ src/ephem/elements.cpp
@@ -0,0 +1,76 @@
+// elements.cpp - orbital elements package
+//
+// Author:  Tim Stark
+// Date:    Apr 25, 2022
+
+#include "main/core.h"
+#include "ephem/elements.h"
+
+const double G       = 6.67259e-11; // gravitational constant [m^3 kg^-1 s^-2]
+const double MJD2000 = 51544.5;     // MJD date for epoch J2000
+// const double pi      = 3.14159265358979323846;
+
+OrbitalElements::OrbitalElements(double a, double e, double i,
+    double theta, double omegab, double L, double mjd)
+: a(a), e(e), i(i), theta(theta), omegab(omegab), L(L),
+  mjdEpoch(mjd)
+{ }
+
+void OrbitalElements::setMasses(double _m, double _M)
+{
+    m  = _m;
+    M  = _M;
+    mu = G * (m + M);
+}
+
+// determine orbital data (free fall) by using masses
+void OrbitalElements::setup(double m, double M, double mjd)
+{
+    setMasses(m, M);
+
+    le = a * e;     // linear eccentricity
+    ad = a + le;    // apoapsis distance
+    pd = a - le;    // periapsis distance
+    omega = omegab - theta;
+
+    if (e < 1.0)
+    {
+        // circular/elliptical orbit
+        b = a * sqrt(1.0 - e*e);
+        n = sqrt(mu / (a*a*a));
+        tmp = sqrt((1.0 + e) / (1.0 - e));
+    }
+    else
+    {
+        // parabolic/hyperbolic orbit
+        b = a * sqrt(e*e - 1.0);
+        n = sqrt(-mu / (a*a*a));
+    }
+
+    // set time parameters
+    double mjdStart = 0.0;
+    T = (pi * 2.0) / n;                  // orbital period
+    L += (mjd - mjdEpoch) * 86400.0 * n; // mean longitude
+    mjdEpoch = mjd;
+    tEpoch = (mjdEpoch - mjdStart) * 86400.0;
+    tau = tEpoch - (L - omegab) / n;
+}
+
+// determine new orbital path
+void OrbitalElements::calculate(const vec3d_t &pos, const vec3d_t &vel, double t)
+{
+    // Set radius/velocity vectors
+    R = pos;
+    V = vel;
+    r = R.length();
+    v = V.length();
+
+    // a = r * mu / (2.0 * mu - r * V.squaredNorm());
+
+}
+
+// updating position and velocity periodically
+void OrbitalElements::update(vec3d_t &pos, vec3d_t &vel)
+{
+
+}
---
+++ src/ephem/elements.h
@@ -0,0 +1,75 @@
+// elements.h - orbital elements package
+//
+// Author:  Tim Stark
+// Date:    Apr 25, 2022
+
+#pragma once
+
+class OrbitalElements
+{
+public:
+    OrbitalElements() = default;
+    ~OrbitalElements() = default;
+
+    OrbitalElements(double a, double e, double i, double theta, double omegab, double L, double mjd);
+
+    inline double getLinearEccentricity() const  { return le; }
+    inline double getApoapsis() const            { return ad; }
+    inline double getPeriapsis() const           { return pd; }
+    inline double getSemiMinorAxis() const       { return b; }
+    inline double getArgumentOfPeriapsis() const { return omega; }
+    inline double getOrbitalPeriod() const       { return T; }
+
+    inline double getMJDEpoch() const            { return mjdEpoch; }
+    
+    void setMasses(double m, double M);
+    double getCircularVelocity(double r) const   { return sqrt(mu / r); };
+
+    void setup(double m, double M, double mjd);
+    void calculate(const vec3d_t &pos, const vec3d_t &vel, double t);
+    void update(vec3d_t &pos, vec3d_t &vel);
+
+public:
+    // Primary orbital element parameters
+    double a      = 1.0;    // semi-major axis [m]
+    double e      = 0.0;    // numerical eccentricity
+    double i      = 0.0;    // inclination [rad]
+    double theta  = 0.0;    // longitude of ascending node [rad]
+    double omegab = 0.0;    // longitude of periapsis [rad]
+    double L      = 0.0;    // mean longitude at epoch
+
+private:
+    // Secondary orbital element parameters
+    double le;          // linear eccentricity [m]
+    double ad;          // apoapsis distance [m]
+    double pd;          // periapsis distance [m]
+    double b;           // Semi-minor axis [m]
+    double omega;       // Argument of periapsis [rad]
+    double n;           // 2pi/T
+    double tmp;         // calculation of true anomaly if e < 1
+
+    // Mass parameters
+    double m;           // mass of orbiter [kg]
+    double M;           // mass of central object [kg]
+    double mu;          // standard gravitational parameter G * (M+m)
+
+    // Time parameters
+    double T;           // orbital period [s]
+    double Tpe;         // time to next periaspsis passage [s]
+    double Tap;         // time to bext apoaspsis passage [s]
+    double tau;         // periapsis passage [s]
+    
+    double mjdEpoch;    // reference time (MJD format)
+    double tEpoch;      // reference time
+
+    // position parameters (only valid during time through update call)
+    vec3d_t R;          // radius (position) vector
+    vec3d_t V;          // velocity vector
+    double  r;          // radius (position) vector length
+    double  v;          // magnitude of velocity
+    double  ea;         // eccentric anomaly
+    double  ma;         // mean anomaly
+    double  tra;        // true anomaly
+    double  ml;         // mean longitude
+    double  trl;        // true longitude
+};
