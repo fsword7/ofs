2407b97c3b6c41b51d1cd666e97e7aa10903927c
Timothy Stark <fsword007@gmail.com>
2023-01-16 16:01:39 -0500

    VSOP87 updates (in progress)...

--- src/tools/vsop87/vsop87.cpp
+++ src/tools/vsop87/vsop87.cpp
@@ -1,13 +1,11 @@
-/*
- * vsop87sph.cpp - VSOP87 C++ data generator package
- *
- * VSOP87 package are available for downloading:
- *
- * ftp://ftp.imcce.fr/pub/ephem/planets/vsop87/
- *
- *  Created on: Mar 10, 2020
- *      Author: Tim Stark
- */
+// vsop87.cpp - VSOP87 C++ data generator package
+//
+// VSOP87 package are available for downloading:
+//
+// ftp://ftp.imcce.fr/pub/ephem/planets/vsop87/
+//
+// Author:	Tim Stark
+// Date:	Jan 7, 2023
 
 #include <iostream>
 #include <fstream>
@@ -20,14 +18,14 @@ namespace fs = std::filesystem;
 
 void usage(char *name)
 {
-	cout << fmt::format("Usage: {} <planet name>\n", name);
+	cout << fmt::format("Usage: {} <path> [planet name] [data type]\n", name);
 	exit(1);
 }
 
-void print(double data[2000][3], const char *planet, int lbr, int deg, int terms)
+void print(double data[3000][3], const char *planet, int set, int elm, int deg, int terms)
 {
 	cout << fmt::format("static vsop87_t {}_{}{}[{}] = {{\n",
-		planet, "lbr"[lbr], deg, terms);
+		planet, "lbr"[elm], deg, terms);
 
 	for (int idx = 0; idx < terms; idx++) {
 		cout << fmt::format("\t{{ {:18.12f}, {:18.12f}, {:20.12f} }}{}\n",
@@ -38,39 +36,8 @@ void print(double data[2000][3], const char *planet, int lbr, int deg, int terms
 	cout << fmt::format("}};\n\n");
 }
 
-int main(int argc, char **argv)
+void read(fs::path fname, const char *planet)
 {
-	const char *list = nullptr;
-
-	if (argc > 1)
-		list = argv[1];
-	else
-		usage(argv[0]);
-
-	fs::path path(list);
-	for(auto &file : fs::recursive_directory_iterator(list,
-		fs::directory_options::skip_permission_denied))
-	{
-		fs::path infile = file;
-
-		cout << infile.string() << endl;
-	}
-
-	exit(EXIT_SUCCESS);
-}
-
-int oldmain(int argc, char **argv)
-{
-	const char *planet = "earth"; // default planet name
-
-	if (argc > 1)
-		planet = argv[1];
-	else
-		usage(argv[0]);
-
-	string fname = "VSOP87B." + string(planet, 0, 3);
-//	cout << "Generating VSOP87 data terms from " << fname << " for " << planet << "..." << endl;
-
 	ifstream vsopFile(fname, ios::in);
 	if (!vsopFile.is_open()) {
 		cerr << "File " << fname << ": " << strerror(errno) << endl;
@@ -78,32 +45,42 @@ int oldmain(int argc, char **argv)
 	}
 
 	string line;
+	string type;
 	int lnum = 0;
+	int dset = 0;
 	int terms = 0;
-	int lbr, deg;
-	double data[2000][3];
+	int elm, deg, emax;
+	double data[3000][3];
 
 	while (getline(vsopFile, line)) {
 		lnum++;
+		if (line.empty())
+			continue;
 		const char *cline = line.c_str();
 		if (!strncmp(cline, " VSOP87", 7)) {
 			if (terms > 0)
-				print(data, planet, lbr, deg, terms);
+			{
+				cout << "Set " << dset << " Element: " << elm << " Degree: " << deg << " Terms: " << terms << " Type: " << type << endl;
+				print(data, planet, dset, elm, deg, terms);
+			}
+			dset++;
 			terms = 0;
 
-			lbr = (int)cline[41] - (int)'1';
+			type = line.substr(44, 3);
+			elm = (int)cline[41] - (int)'1';
 			deg = (int)cline[59] - (int)'0';
+			emax = type == "ALK" ? 6 : 3;
 
 			if (deg < 0 || deg > 5) {
 				cout << "Bad degree (" << deg << ") in VSOP data file at line "
 					 << lnum << endl;
-				exit(1);
+				break;
 			}
 
-			if (lbr < 0 || lbr > 3) {
-				cout << "Bad data type (" << lbr << ") in VSOP data file at line "
+			if (elm < 0 || elm > emax) {
+				cout << "Bad data type (" << elm << ") in VSOP data file at line "
 					 << lnum << endl;
-				exit(1);
+				break;
 			}
 
 		} else {
@@ -112,7 +89,7 @@ int oldmain(int argc, char **argv)
 
 			if (sscanf(cline+80, " %lf %lf %lf", &a, &b, &c) != 3) {
 				cout << "Bad data in VSOP data at line " << lnum << endl;
-				exit(1);
+				break;
 			}
 
 			data[terms][0] = a;
@@ -123,8 +100,48 @@ int oldmain(int argc, char **argv)
 	}
 
 	if (terms > 0)
-		print(data, planet, lbr, deg, terms);
+	{
+		cout << "// Set " << dset << " Element: " << elm << " Degree: " << deg << " Terms: " << terms << " Type: " << type << endl;
+		print(data, planet, dset, elm, deg, terms);
+	}
 
 	vsopFile.close();
-	exit(1);
+}
+
+int main(int argc, char **argv)
+{
+	const char *list = nullptr;
+	const char *planet = "earth"; // default planet 
+	const char *dtype = "C"; // default type (LBR data type)
+	std::string fext;
+
+	if (argc > 1)
+	{
+		list = argv[1];
+		if (argc > 2)
+			planet = argv[2];
+		if (argc > 3)
+			dtype = argv[3];
+
+		fext = "." + std::string(planet, 0, 3);
+	}
+	else
+		usage(argv[0]);
+
+	fs::path path(list);
+	for(auto &file : fs::recursive_directory_iterator(list,
+		fs::directory_options::skip_permission_denied))
+	{
+		fs::path infile = file;
+		// cout << infile.string() << ": " << infile.extension() << endl;
+		if (infile.extension() != fext)
+			continue;
+		cout << infile.filename() << endl;
+		if (infile.filename().string()[6] != *dtype)
+			continue;
+		cout << infile.string() << endl;
+		read(infile, planet);
+	}
+
+	exit(EXIT_SUCCESS);
 }
