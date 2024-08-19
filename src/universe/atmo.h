00f2d56a0b30b2a997f666865cd80c235a423302
Timothy Stark <fsword007@gmail.com>
2024-08-19 11:24:43 -0400

    Added atmconst_t package for surface forces.

--- src/universe/atmo.h
+++ src/universe/atmo.h
@@ -5,28 +5,48 @@
 
 #pragma once
 
+struct atmconst_t
+{
+    color_t color0;         // Sky color at sea level (daytime)
+    double  altLimit;       // Atomsphere atitude limit
+    double  radLimit;       // Radius limit (Mean radius + atomsphere limit)
+    double  hAlt;           // horizon rendering altitude
+
+    double  p0;             // air pressure at mean radius (sea level)
+    double  rho0;           // density at mean radius
+    double  R;              // specific gas constant
+    double  gamma;          // ratio of specific heats
+    double  C;              // exponent for pressure equation
+    double  O2app;          // partial pressure of oxygen
+};
+
+// Atmospheric parameters (input)
+struct iatmprm_t
+{
+    double alt;         // Altitude [km]
+    double lat;         // Latitude [radians]
+    double lng;         // Longtitude [radians]
+    // double ap;          // Magnetic index
+    // double flux;        // Current flux
+    // double fluxa;       // Average flux
+    uint32_t flags;     // Parameter flags
+};
+
+// Atmospheric parameters (results)
+struct atmprm_t
+{
+    double p;           // Pressure [Pa]
+    double rho;         // Density [kg/m^3]
+    double T;           // Temperature [K]
+};
+
 class Atomsphere
 {
 public:
-    // Atmospheric parameters (input)
-    struct inAtmParam
-    {
-        double alt;         // Altitude [km]
-        double lat;         // Latitude [radians]
-        double lng;         // Longtitude [radians]
-        // double ap;          // Magnetic index
-        // double flux;        // Current flux
-        // double fluxa;       // Average flux
-        uint32_t flags;     // Parameter flags
-    };
+    Atomsphere() = default;
 
-    // Atmospheric parameters (results)
-    struct AtmParam
-    {
-        double p;           // Pressure [Pa]
-        double rho;         // Density [kg/m^3]
-        double T;           // Temperature [K]
-    };
+    virtual void getAtmConstants(atmconst_t &atmc) = 0;
+    virtual void getAtmParams(const iatmprm_t &in, atmprm_t &out) = 0;
 
 protected:
     double altLimit;        // Altitutde limit [km]
