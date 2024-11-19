55f1066c4998d9886aae610f45fd329860330d49
Timothy Stark <fsword007@gmail.com>
2024-11-19 08:32:26 -0500

    Updates.

--- src/engine/radio.h
+++ src/engine/radio.h
@@ -9,11 +9,24 @@
 #define NAV_FREQ_MAX    137.0
 #define NAV_FREQ_NSTEP  640
 
+class CelestialPlanet;
+class Base;
+
+enum rnavType
+{
+    rnavNone = 0,
+    rnavVOR,        // VHF Omnidirectional Range 
+    rnavVTOL,       // Vertical Takeoff/Landing
+    rnavILS,        // Instrument Landing System
+    rnavIDS         // Instrument Docking System
+};
+
 class RadioNavigation
 {
 public:
     RadioNavigation(float freq = NAV_FREQ_MIN, float range = 500);
 
+    inline rnavType getType() const     { return type; }
     inline float getFreq() const        { return freq; }
     inline float getRange() const       { return range; }
     inline int getStep() const          { return step; }
@@ -31,10 +44,46 @@ public:
     virtual str_t getID(cstr_t &name) = 0;
 
 protected:
+    rnavType type = rnavNone;
+    float range = 500;
     float freq;
-    float range;
     int step;
     cstr_t id;
+};
+
+// Very High Frequency Omnidirectional Range Station (VOR)
+class RadioVOR : public RadioNavigation
+{
+public:
+    RadioVOR(CelestialPlanet *planet);
+    RadioVOR(CelestialPlanet *planet, double lat, double lng);
+    RadioVOR(CelestialPlanet *planet, double lat, double lng, float freq, float range = 500);
+    RadioVOR(CelestialPlanet *planet, cstr_t &name);
+
+    inline CelestialPlanet *getPlanet() const   { return planet; }
+    inline glm::dvec3 getPosition() const       { return lpos; }
+    inline glm::dvec3 getLocation() const       { return { lat, lng, 0 }; }
+
+protected:
+    CelestialPlanet *planet;
+    double lat, lng;
+    glm::dvec3 lpos;
+};
+
+// Instrument Landing System (ILS)
+class RadioILS : public RadioNavigation
+{
+public:
+    RadioILS(Base *base, double lat, double lng, double dir, float freq, float range = 500);
+
+    inline double getDirection() const          { return dir; }
+    // inline glm::dvec3 getPosition() const       { return lpos; }
+    inline glm::dvec3 getLocation() const       { return { lat, lng, 0 }; }
+
+protected:
+    Base *base;
+    double lat, lng;
+    double dir;
 };
 
 class NavigationManager
